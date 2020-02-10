f1cff6aa326a440754f13d36e39b2872a92dfcbc
burak_ata <hiyelata@yahoo.com>
2020-02-11 00:26:41 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { Route, NavLink, HashRouter } from 'react-router-dom';
+import BoksHakkında from './compo/BoksHakkında.js';
+import Sparring from './compo/Sparring.js';
+import Analiz from './compo/Analiz.js';
+import Kariyer from './compo/Kariyer.js';
+import Antrenman from './compo/Antrenman.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <HashRouter>
+        <div className="App">
+          <h1 className="üst-fotolar"></h1>
+
+          <ul className="header">
+            <a href=".boxing" id="boxing"><span className="boxing">Boxing-TR</span></a>
+            <li><NavLink exact to="/">Boks Hakkında</NavLink></li>
+            <li><NavLink to="/Sparring">Sparring Buluşması</NavLink></li>
+            <li><NavLink to="/Analiz">Şampiyonların Teknik Analizi</NavLink></li>
+            <li><NavLink to="/Kariyer">Boksta Kariyer</NavLink>></li>
+            <li><NavLink to="/Antrenman">Baştan Ayağa Antrenman</NavLink></li>
+
+          </ul>
+
+          <div className="content">
+            <Route exact path="/" component={
+              BoksHakkında} />
+            <Route path="/Sparring" component={
+              Sparring} />
+            <Route path="/Analiz" component={
+              Analiz} />
+            <Route path="/Kariyer" component={
+              Kariyer} />
+            <Route path="/Antrenman" component={Antrenman} />
+          </div>
+
+        </div>
+      </HashRouter>
+
+    );
+  }
 }
-
 export default App;
